666ea2f7d36e7d975960e4ebd6d2a830a3348872
Aljoscha Nyang <a.nyang@gmx.de>
2022-08-22 21:47:05 +0200

    Connect buttons with function

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,50 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+// void main() {
+//   runApp(MyApp());
+// }
+
+// alternative syntax
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  var questionIndex = 0;
+
+  void answerQuestion() {
+    questionIndex = questionIndex + 1;
+    print(questionIndex);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's yout favorite animal",
+    ];
+
     return MaterialApp(
-      home: Text("Hello!"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My First App"),
+        ),
+        body: Column(
+          children: [
+            Text(questions[questionIndex]),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Button1"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Button2"),
+            ),
+            RaisedButton(
+              onPressed: answerQuestion,
+              child: Text("Button3"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
