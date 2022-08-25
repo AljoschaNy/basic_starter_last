024d2ce66f3ff56cf8d681a071073b53501bfcdc
Aljoscha Nyang <a.nyang@gmx.de>
2022-08-25 14:04:47 +0200

    Output result phrase based on totalscore

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+
+  Result(this.resultScore);
+
+  String get resultPhrase {
+    String resultText;
+
+    if (resultScore <= 6) {
+      resultText = "You are so bad!";
+    } else if (resultScore <= 10) {
+      resultText = "You are ... strange?!";
+    } else if (resultScore <= 20) {
+      resultText = "Pretty likeable!";
+    } else {
+      resultText = "You are awesome and innocent";
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("You are done"),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
